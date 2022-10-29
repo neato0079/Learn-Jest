29e1b8345ed1e4c191a9a3900a2362cdaa73fa55
neato0079 <neato0079@gmail.com>
2022-10-29 14:27:22 -0700

    progress

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const axios = require('axios')
 const axiosRetry = require('axios-retry')
+const retry = require('retry')
 
-const url200 = 'http://webcode.me'
+const url200 = 'https://httpstat.us/200'
 const url401 = 'https://httpstat.us/401'
 
 
-axiosRetry(axios, {
-  retries: 3, // number of retries
-  retryDelay: (retryCount) => {
-    console.log(`retry attempt: ${retryCount}`);
-    return retryCount * 2000; // time interval between retries
-  },
-  // retryCondition: (error) => {
-  //     // if retry condition is not specified, by default idempotent requests are retried
-  //     return error.response.status === 503;
-  // },
-});
 
 const arrayLength = (array) => array.length;
 const getAxiosCall = async (url) => {
+  axiosRetry(axios, {
+    retries: 2, // number of retries
+    retryDelay: (retryCount) => {
+      console.log(`retry attempt: ${retryCount}`);
+      return retryCount * 1000; // time interval between retries
+    },
+    // retryCondition: (error) => {
+    //     // if retry condition is not specified, by default idempotent requests are retried
+    //     return error.response.status === 503;
+    // },
+  });
   // try{
-  const result = await axios.get(url).then(() => console.log('trying...')).catch((err) => {
+  const result = await axios.get(url).catch((err) => {
     if (err.response.status !== 200) {
       throw new Error(`API call failed with status code: ${err.response.status} after 3 retry attempts`);
     }
@@ -46,20 +47,72 @@ const getAxiosCall = async (url) => {
 //   });
 // }
 
-const fuckit = async () => {
-  try {
-    const response = await axios.get('https://httpstat.us/501');
-    return response
-  } catch(e) {
-    throw e
-  }
+// const tryToRetry = async () => {
+//   try {
+//     const response = await axios.get('https://httpstat.us/503').catch((e) => {console.log('in .catch block')})
+//     console.log('In try block')
+//     return response
+//   } catch (e) {
+//     console.log('In catch block')
+//     throw await e
+//   }
+// }
+// try/catch blocks arent working. try without
+
+const tryToRetry = async () => {
+  axiosRetry(axios, {
+    retries: 2, // number of retries
+    retryDelay: (retryCount) => {
+      console.log(`retry attempt: ${retryCount}`);
+      return retryCount * 1000; // time interval between retries
+    },
+    // retryCondition: (error) => {
+    //     // if retry condition is not specified, by default idempotent requests are retried
+    //     return error.response.status === 503;
+    // },
+  });
+  const response = await axios.get('https://httpstat.us/401').catch((e) => { console.log(`in catch block\n${e}`) })
+  console.log(response)
+  return response
 }
 
-fuckit()
+
+// retry lib
+const retryLibTry = async () => {
+  const operation = retry.operation({
+    retries: 1,
+    factor: 3,
+    minTimeout: 1 * 1000,
+    maxTimeout: 60 * 1000,
+    randomize: true,
+  });
+
+  operation.attempt(async (currentAttempt) => {
+    console.log('sending request: ', currentAttempt, ' attempt');
+    try {
+
+      const response = await axios.get(url401);
+      console.log('RESOLVE')
+      return 'nice'
+
+    } catch (e) {
+      if (!operation.retry(e)) {
+        console.log(e.code)
+        return 'FAILURE LOL';
+      }
+    }
+  });
+
+}
+
+// retryLibTry()
+tryToRetry()
 // makeHTTPCall();
 // getAxiosCall(url401)
 
 module.exports = {
   arrayLength,
-  getAxiosCall
+  getAxiosCall,
+  tryToRetry,
+  retryLibTry
 }
--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
 const index = require('./index')
 const mockAxios = require('axios')
 const { default: axios } = require('axios')
+const axiosRetry = require('axios-retry')
+
 jest.mock("axios")
 afterEach(() => {
   jest.resetAllMocks();
@@ -8,30 +10,46 @@ afterEach(() => {
 
 describe('test', () => {
   test('Returns value of 3 for a array with a size of 3', () => {
-    const array = [0,0,0]
+    const array = [0, 0, 0]
     const empty = []
     const result = index.arrayLength(array)
     const emptyResult = index.arrayLength(empty)
     expect.assertions(2)
     expect(result).toBe(3)
     expect(emptyResult).toBeFalsy()
+  });
+
+  test('Axios call returns status:200', async () => {
+    mockAxios.get.mockResolvedValue({ status: 200 });
+    result = index.getAxiosCall();
+    expect.assertions(2);
+    expect(result).resolves.toBeDefined();
+    expect(result).resolves.toEqual(200);
+  });
+
+  test('Axios call handles error', async () => {
+    mockAxios.get.mockRejectedValue(new Error({}));
+    result = index.getAxiosCall();
+    expect.assertions(2);
+    await expect(result).rejects.toBeDefined();
+    expect(result).rejects.toThrow({});
+  });
+
+  test('Axios retries 3 times using the axios-retry lib', async () => {
+    // jest.fn(mockAxios.get.mockRejectedValue(new Error({})))
+    
+    mockAxios.get.mockRejectedValue(new Error('hi'))
+    
+    
+    await index.tryToRetry(); // using axios-retry lib
+    // expect(mockAxios.get).toHaveBeenCalledTimes(3)
+    expect(mockAxios.get).toReturnWith('hi')
+    // expect(mockAxios.get).toHaveNthReturnedWith(1)
   })
 
-  test('Axios call returns status:200', async() => {
-    mockAxios.get.mockResolvedValue({status: 200})
-    result = index.getAxiosCall()
-    expect.assertions(2)
-    expect(result).resolves.toBeDefined()
-    expect(result).resolves.toEqual(200)
-  })
-
-  test('Axios call handles error', async() => {
-    // const url200 = 'http://webcode.me'
-    // const url500 = 'https://no-such-server.blabla'
-    mockAxios.get.mockRejectedValue(new Error('poop'));
-    result = index.getAxiosCall()
-    expect.assertions(2)
-    await expect(result).rejects.toBeDefined()
-    expect(result).rejects.toThrow('poop')
+  test.skip('Axios retries 3 times using the retry lib', async () => {
+    mockAxios.get.mockRejectedValue(new Error({}));
+    await index.retryLibTry(); // using retry lib
+    expect(mockAxios.get).toHaveBeenCalledTimes(3)
   })
 })
